424a8cf6537b8c4375aca867cf21e9b10f5e2ce7
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2019-08-06 17:00:54 +0800

    created migration file for design table.

---
+++ database/migrations/2019_08_06_074630_create_designs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDesignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('design_url', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
